2eb5e2d2ed42fec45404b1fd4ab125814a7a9732
Gabriel <gabrielreyes2318@gmail.com>
2021-09-19 23:50:27 -0500

    Graficadora Terminada

--- js/graf.js
+++ js/graf.js
@@ -1,36 +1,177 @@
+let diagBarBtn = document.querySelector('#diag-bar');
+let diagPieBtn = document.querySelector('#diag-pie');
+let diagLineBtn = document.querySelector('#diag-line');
 
-var ctx= document.getElementById("grafic").getContext("2d");
-const d1= document.getElementById("d1");
-const d2= document.getElementById("d2");
-const d3= document.getElementById("d3");
+let graficContext = document.querySelector('#grafic').getContext("2d");
 
+let chart;
 
-var g= new Chart(ctx,{
-	type:"bar",
-		data:{
-			labels:['col1','col2','col3'],
-			datasets:[{
-				label:'Num datos',
-				data:[10,9,15],
-				backgroundColor:[
-				'rgb(66, 134, 244,0.2)',
-				'rgb(74, 135, 72,0.2)',
-				'rgb(229, 89, 50,0.2)'
-				],
-				borderColor:[
-					'rgb(66, 134, 244)',
-					'rgb(74, 135, 72)',
-					'rgb(229, 89, 50)'
-				]
-			}]
-		},
-	options:{
-		scales:{
-			yAxes:[{
-			ticks:{
-				beginAtZero:true
-				}
-			}]
-		}
+let datosInput;
+let datos = [];
+let labels = [];
+let data = [];
+//Funciones
+
+function recibirDatos(){
+	labels.length = 0;
+	data.length = 0;
+	
+	datosInput = document.querySelector('#datos-input').value.trim();
+	datosInput = datosInput.replace(/  /g, '')
+
+	datos = datosInput.split(',');
+	datos.forEach(element => {
+		datos[datos.indexOf(element)] = element.split(':')
+	});
+
+	datos.forEach(element => { // organizar labels
+		labels.push(datos[datos.indexOf(element)][0]);
+	});
+
+	datos.forEach(element => { // organizar datos
+		data.push(datos[datos.indexOf(element)][1]);
+	});
+
+}
+//Diagrama Barras
+function createBars(e){
+	e.preventDefault();
+	console.log('Barras');
+
+	if(chart != undefined){
+		chart.destroy()
 	}
+
+	recibirDatos();
+
+	chart = new Chart(graficContext,{
+		type:"bar",
+			data:{
+				labels:labels,
+				datasets:[{
+					label:'n',
+					data:data,
+					backgroundColor:[
+					'rgb(66, 134, 244,0.2)',
+					'rgb(74, 135, 72,0.2)',
+					'rgb(229, 89, 50,0.2)',
+					'rgb(202, 111, 30,0.2)',
+					'rgb(244, 208, 63,0.2)',
+					'rgb(125, 60, 152,0.2)',
+					'rgb(34, 153, 84,0.2)',
+					'rgb(131, 145, 146,0.2)',				
+					
+					],
+					borderColor:[
+						'rgb(26, 188, 156,0.6)'
+					]
+				}]
+			},
+		options:{
+			scales:{
+				yAxes:[{
+				ticks:{
+					beginAtZero:true
+					}
+				}]
+			}
+		}
 });
+}
+//Diagrama Pie
+function createPie(e){
+	e.preventDefault();
+	console.log('Pie');
+		if(chart != undefined){
+		chart.destroy()
+	}
+
+	recibirDatos();
+
+	chart = new Chart(graficContext,{
+			type:"pie",
+				data:{
+					labels:labels,
+					datasets:[{
+						label:'n',
+						data:data,
+						backgroundColor:[
+							'rgb(66, 134, 244,0.2)',
+							'rgb(74, 135, 72,0.2)',
+							'rgb(229, 89, 50,0.2)',
+							'rgb(202, 111, 30,0.2)',
+							'rgb(244, 208, 63,0.2)',
+							'rgb(125, 60, 152,0.2)',
+							'rgb(34, 153, 84,0.2)',
+							'rgb(131, 145, 146,0.2)',				
+							
+							],
+							borderColor:[
+								'rgb(26, 188, 156,0.6)'
+							]					
+						}]
+				}
+	/*
+			options:{
+				scales:{
+					yAxes:[{
+					ticks:{
+						beginAtZero:true
+						}
+					}]
+				}
+			}
+
+	*/
+
+	}); // create chart;
+}
+//Diagrama Lineas
+function createLine(e){
+	e.preventDefault();
+	console.log('Line');
+	if(chart != undefined){
+		chart.destroy()
+	}
+
+	recibirDatos();
+
+	chart = new Chart(graficContext,{
+			type:"line",
+			data:{
+				labels:labels,
+				datasets:[{
+					label:'n',
+					data:data,
+					backgroundColor:[
+						'rgb(66, 134, 244,0.2)',
+						'rgb(74, 135, 72,0.2)',
+						'rgb(229, 89, 50,0.2)',
+						'rgb(202, 111, 30,0.2)',
+						'rgb(244, 208, 63,0.2)',
+						'rgb(125, 60, 152,0.2)',
+						'rgb(34, 153, 84,0.2)',
+						'rgb(131, 145, 146,0.2)',				
+						
+						],
+						borderColor:[
+							'rgb(26, 188, 156,0.6)'
+						]					
+					}]
+			},
+			options:{
+				scales:{
+					yAxes:[{
+					ticks:{
+						beginAtZero:true
+						}
+					}]
+				}
+			}
+	}); // create chart;
+}
+
+//Listeners
+diagBarBtn.addEventListener('click',createBars);
+diagPieBtn.addEventListener('click',createPie);
+diagLineBtn.addEventListener('click',createLine);
